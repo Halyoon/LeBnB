d6bedf8d9fb56b42af193df47d3da7391b63059c
Mahdi Shehadi <mahdishehadi52@gmail.com>
2023-12-01 15:19:20 +0200

    added the home api page

--- back-spring/src/main/java/com/halyoon/app/home/HomeController.java
+++ back-spring/src/main/java/com/halyoon/app/home/HomeController.java
@@ -1,21 +1,30 @@
 package com.halyoon.app.home;
 
+
 import com.halyoon.app.auth.AuthenticationResponse;
 import com.halyoon.app.auth.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
 @RequestMapping(path = "/")
-@RequiredArgsConstructor
 public class HomeController {
-
-
     @GetMapping
-    public ResponseEntity<?> register() {
-        String response ="Welcome to Lebnb API's!!";
+    public String getOrderedList(Model model) {
 
-            return ResponseEntity.ok(response);
+        Map<String, String> items = new HashMap<>();
+        items.put("Stays", "http://localhost:8080/api/stay");
+        items.put("Stays Length", "http://localhost:8080/api/stay/length");
+        items.put("Like a stay", "http://localhost:8080/api/stay/length");
+
+        model.addAttribute("items", items);
+
+        return "api-list";
     }
 }
